485e68a76a10aea916a285b3cce3393357b749b4
egorummy <adam.mokrzycki22@gmail.com>
2025-09-16 17:05:50 +0200

    + clean up

--- src/main/java/com/rummgp/medical_clinic/mapper/PageMapper.java
+++ src/main/java/com/rummgp/medical_clinic/mapper/PageMapper.java
@@ -1,12 +1,12 @@
 package com.rummgp.medical_clinic.mapper;
 
 import com.rummgp.medical_clinic.dto.PageDto;
-import org.mapstruct.Mapper;
 import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Component;
 
 import java.util.function.Function;
 
-@Mapper(componentModel = "spring")
+@Component
 public interface PageMapper {
 
     default <E, D> PageDto<D> toDto(Page<E> page, Function<E, D> mapper) {
