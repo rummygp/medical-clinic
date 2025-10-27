c80079ce12e29ad87de45789b2ad0be65d3e3a86
egorummy <adam.mokrzycki22@gmail.com>
2025-10-27 12:06:41 +0100

    + elk

--- src/test/java/com/rummgp/medical_clinic/controller/AppointmentControllerTest.java
+++ src/test/java/com/rummgp/medical_clinic/controller/AppointmentControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
